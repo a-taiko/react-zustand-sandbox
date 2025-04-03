c1aa7437045898e7242e0550aec174f50e958266
a-taiko <atsumu.taiko@nsw.co.jp>
2025-04-03 14:25:24 +0900

    wrap devtools

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,13 +1,16 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 type counterState = {
   count: number;
-  increment: () => void,
-  decrement: () => void,
+  increment: () => void;
+  decrement: () => void;
 };
 
-export const useCounterStore = create<counterState>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-}));
+export const useCounterStore = create<counterState>()(
+  devtools((set) => ({
+    count: 0,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+    decrement: () => set((state) => ({ count: state.count - 1 })),
+  }))
+);
